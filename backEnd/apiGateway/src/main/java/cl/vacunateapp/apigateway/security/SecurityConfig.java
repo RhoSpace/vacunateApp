901bf33d1f89159e16955b41827e08d41640912b
jebuz <sebaamts@gmail.com>
2023-01-25 00:58:10 -0300

    Actualizacion de configuracion de seguridad

--- backEnd/apiGateway/src/main/java/cl/vacunateapp/apigateway/security/SecurityConfig.java
+++ backEnd/apiGateway/src/main/java/cl/vacunateapp/apigateway/security/SecurityConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -32,8 +31,6 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder managerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
-
-//        managerBuilder.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder);
         managerBuilder.userDetailsService(customUserDetailService).passwordEncoder(passwordEncoder);
 
         AuthenticationManager authenticationManager = managerBuilder.build();
