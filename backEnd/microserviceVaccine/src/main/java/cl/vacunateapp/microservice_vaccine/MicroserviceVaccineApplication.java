f01ab868664721d3a0a102c7d3ce650b643a7323
jebuz <sebaamts@gmail.com>
2023-01-26 14:09:34 -0300

    Creacion de proyecto inicial en spring + configuracion

---
+++ backEnd/microserviceVaccine/src/main/java/cl/vacunateapp/microservice_vaccine/MicroserviceVaccineApplication.java
@@ -0,0 +1,13 @@
+package cl.vacunateapp.microservice_vaccine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceVaccineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceVaccineApplication.class, args);
+    }
+
+}
