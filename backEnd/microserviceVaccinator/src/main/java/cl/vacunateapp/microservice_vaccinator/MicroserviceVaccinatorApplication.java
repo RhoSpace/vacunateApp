be10587e0c4508418d9d3671655367982967249b
jebuz <sebaamts@gmail.com>
2023-01-26 19:47:08 -0300

    Creacion de proyecto inicial en spring

---
+++ backEnd/microserviceVaccinator/src/main/java/cl/vacunateapp/microservice_vaccinator/MicroserviceVaccinatorApplication.java
@@ -0,0 +1,13 @@
+package cl.vacunateapp.microservice_vaccinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceVaccinatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceVaccinatorApplication.class, args);
+    }
+
+}
---
+++ backEnd/microserviceVaccinator/src/test/java/cl/vacunateapp/microservice_vaccinator/MicroserviceVaccinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.vacunateapp.microservice_vaccinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceVaccinatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
