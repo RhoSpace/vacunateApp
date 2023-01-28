2cc46a8e79b1c7ce630966a1c3c4f2d30e05e9c5
jebuz <sebaamts@gmail.com>
2023-01-28 20:51:21 -0300

    Añadido decorador para FeignClients

--- backEnd/apiGateway/src/main/java/cl/vacunateapp/apigateway/ApiGatewayApplication.java
+++ backEnd/apiGateway/src/main/java/cl/vacunateapp/apigateway/ApiGatewayApplication.java
@@ -2,11 +2,13 @@ package cl.vacunateapp.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableFeignClients
 @SpringBootApplication
 public class ApiGatewayApplication {
 
