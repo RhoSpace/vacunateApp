0c139febad37c0c9a1a4bf2dfa533fb18e421173
jebuz <sebaamts@gmail.com>
2023-01-29 15:08:29 -0300

    Bean para passwordEncoder

--- backEnd/microserviceVaccinator/src/main/java/cl/vacunateapp/microservice_vaccinator/MicroserviceVaccinatorApplication.java
+++ backEnd/microserviceVaccinator/src/main/java/cl/vacunateapp/microservice_vaccinator/MicroserviceVaccinatorApplication.java
@@ -2,12 +2,19 @@ package cl.vacunateapp.microservice_vaccinator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class MicroserviceVaccinatorApplication {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 
-    public static void main(String[] args) {
-        SpringApplication.run(MicroserviceVaccinatorApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceVaccinatorApplication.class, args);
+	}
 
 }
